13e85259b945bb671e9a07b284f206fc490e0244
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-05-16 18:07:13 +0200

    updated images

--- tests/factory.js
+++ tests/factory.js
@@ -1,10 +1,24 @@
 function shoeFactory() {
-    var shoeList = [{brand: "Nike", price: "R1299.00", size: 8, stock: 4}, 
-    {brand: "Nike", price: "R1299.00", size: 8, stock: 4}, 
-    {brand: "Nike", price: "R1299.00", size: 8, stock: 4}, 
-    {brand: "Nike", price: "R1299.00", size: 8, stock: 4}, 
-    {brand: "Nike", price: "R1299.00", size: 8, stock: 4}, 
-    {brand: "Nike", price: "R1299.00", size: 8, stock: 4}]
+    var shoeList = [{image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2159410/original.jpg", brand: "Adidas", price: "R2599.00", size: 9, stock: 3}, 
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2075782/original.jpg", brand: "Adidas", price: "R2599.99", size: 8, stock: 3}, 
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2054099/original.jpg", brand: "Adidas", price: "R2599.99", size: 7, stock: 3}, 
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2155221/original.jpg", brand: "Adidas", price: "R1499.99", size: 5, stock: 3}, 
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2066888/original.jpg", brand: "Adidas", price: "R1499.99", size: 10, stock: 3}, 
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/1932843/original.jpg", brand: "Adidas", price: "R1499.99", size: 9, stock: 3},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/1931440/original.jpg", brand: "Nike", price: "R1599.99", size: 9, stock: 6},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2188959/original.jpg", brand: "Nike", price: "R1999.99", size: 8, stock: 6},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/1885442/original.jpg", brand: "Nike", price: "R1999.99", size: 7, stock: 6},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/1896913/original.jpg", brand: "Nike", price: "R1999.99", size: 6, stock: 6},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2049017/original.jpg", brand: "Nike", price: "R1899.99", size: 9, stock: 6},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2023956/original.jpg", brand: "Nike", price: "R1499.99", size: 8, stock: 6},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2052855/original.jpg", brand: "Asics", price: "R1299.99", size: 9, stock: 2},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/1809575/original.jpg", brand: "Asics", price: "R1399.99", size: 8, stock: 2},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2074046/original.jpg", brand: "Converse", price: "R999.99", size: 8, stock: 3},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/678663/original.jpg", brand: "Converse", price: "R999.99", size: 9, stock: 3},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/1702002/original.jpg", brand: "Converse", price: "R999.99", size: 7, stock: 3},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2171254/original.jpg", brand: "New Balance", price: "R1299.99", size: 8, stock: 3},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2133974/original.jpg", brand: "New Balance", price: "R1299.99", size: 9, stock: 3},
+    {image: "https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/2171200/original.jpg", brand: "New Balance", price: "R1299.99", size: 8, stock: 3}]
 
     function values() {
         return {
